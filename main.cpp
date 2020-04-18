7a0df35b5d0abfd65017d4694e58e61b0ac7cc0c
Eilon Cohen <eiloncohen20@gmail.com>
2020-04-18 22:08:23 +0300

    updated main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "textbox.h"
+#include "label.h"
 
 #define TBC (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY | BACKGROUND_BLUE)
 #define REG (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY)
@@ -9,9 +10,17 @@
 
 int main()
 {
+	// label test
+	label l = new label("my new label" , {10,7}); 
+    l.printLableToConsole();
+    l.setColor(FOREGROUND_BLUE , BACKGROUND_RED);
+    while(1); 
 
+	// textBox test
+	
 	COORD tb_size = {TB_WIDTH, TB_HEIGHT};
 	COORD tb_pos = {SCREEN_POS_X, SCREEN_POS_Y};
+
 
 	TextBox text_box(tb_size, tb_pos);
 	printTitle();
